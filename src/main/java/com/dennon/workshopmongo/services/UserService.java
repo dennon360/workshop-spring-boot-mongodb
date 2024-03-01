45f2e9ec83199c3264cb464c9928c335003de633
Dennis Martins <dennis.furtado@vibetecnologia.com>
2024-03-01 12:33:14 -0300

    Obtendo um usuário por id

--- src/main/java/com/dennon/workshopmongo/services/UserService.java
+++ src/main/java/com/dennon/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.dennon.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dennon.workshopmongo.domain.User;
 import com.dennon.workshopmongo.repository.UserRepository;
+import com.dennon.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,9 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
