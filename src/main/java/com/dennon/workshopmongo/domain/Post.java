258631cdc3d144d3adfe143e38b71a9ee3959da7
Dennis Martins <dennis.furtado@vibetecnologia.com>
2024-03-02 12:40:03 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/dennon/workshopmongo/domain/Post.java
+++ src/main/java/com/dennon/workshopmongo/domain/Post.java
@@ -1,25 +1,30 @@
 package com.dennon.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.dennon.workshopmongo.dto.AuthorDTO;
+import com.dennon.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	
+
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -64,6 +69,22 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -79,13 +100,5 @@ public class Post implements Serializable {
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}
-
-	public AuthorDTO getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(AuthorDTO author) {
-		this.author = author;
 	}
 }
