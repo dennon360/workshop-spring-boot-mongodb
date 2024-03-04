9c1f205db67e5dbac6aff0c692da0acc738aa659
Dennis Martins <dennis.furtado@vibetecnologia.com>
2024-03-04 20:28:50 -0300

    Consulta com vários critérios

--- src/main/java/com/dennon/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dennon/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.dennon.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -7,10 +8,13 @@ import org.springframework.data.mongodb.repository.Query;
 
 import com.dennon.workshopmongo.domain.Post;
 
-public interface PostRepository extends MongoRepository<Post, String>{
-	
-	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-	public List<Post> searchTitle(String text);
+public interface PostRepository extends MongoRepository<Post, String> {
 
-	public List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String next, Date minDate, Date maxDate);
 }
