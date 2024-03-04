bec73f478c5b087fbfac32d09719ab25938a34be
Dennis Martins <dennis.furtado@vibetecnologia.com>
2024-03-04 19:30:18 -0300

    Consulta simples com query methods

--- src/main/java/com/dennon/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/dennon/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.dennon.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.dennon.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	public List<Post> findByTitleContainingIgnoreCase(String text);
 }
